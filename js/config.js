12c495d6c9719dac7459682973b7a9a5c5b6f2f5
r1ch <rich@bradi.sh>
2020-03-31 15:12:33 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.config = {
     roleArn: "arn:aws:iam::312837731527:role/GoogleAssumeWITBRole",
     region: "eu-west-1",
-    apiGatewayUrl : "",
+    apiGatewayUrl : "https://nqn8r5yyr6.execute-api.eu-west-1.amazonaws.com/live",
     apiGatewayPath : "/live",
     socketGatewayUrl : ""
 }
