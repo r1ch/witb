4a069136f2cd3aa720263a2171e88592570bc484
r1ch <rich@bradi.sh>
2020-03-31 14:22:06 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.config = {
-    roleArn: "arn:aws:iam::312837731527:role/GoogleAsuumeWITBRole",
+    roleArn: "arn:aws:iam::312837731527:role/GoogleAssumeWITBRole",
     region: "eu-west-1",
     apiGatewayUrl : "",
     apiGatewayPath : "/live",
