c9bdb2fe91bdafacf3545727dc50d9b8bd9a3786
r1ch <rich@bradi.sh>
2020-03-31 15:38:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 Vue.component('google-login', {
 	data: () => ({
 		authenticated: false
+		profileURL : false
 	}),
 	template: `
 		<div class = "row">
 			<div v-if = "!authenticated" class="g-signin2" data-width="200" data-height="50" data-onsuccess="authenticate" data-theme="dark"></div>
+			<img :src="profileURL"></img>
 		</div>
 	`,
 	mounted: function() {
 		Credentials.then(() => {
 			this.authenticated = true
+			this.profileURL = profile.getImageUrl()
 		})
 	}
 })
