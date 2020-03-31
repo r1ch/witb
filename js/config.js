d76b5240e927eac8eaec26ec3455a82bc757af36
r1ch <rich@bradi.sh>
2020-03-31 14:20:48 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window.config = {
+    roleArn: "arn:aws:iam::312837731527:role/GoogleAsuumeWITBRole",
+    region: "eu-west-1",
+    apiGatewayUrl : "",
+    apiGatewayPath : "/live",
+    socketGatewayUrl : ""
+}
