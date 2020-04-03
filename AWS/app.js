b034f43a39ad4758fcc014577e0100c549a5a82b
r1ch <rich@bradi.sh>
2020-04-03 10:51:39 +0100

    Auto deploy

--- AWS/app.js
+++ AWS/app.js
@@ -1,4 +1,11 @@
 'use strict'
+//AWS Setup
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient()
+
+
+
+//Express Setup
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -13,8 +20,12 @@ router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 router.use(awsServerlessExpressMiddleware.eventContext())
 
-router.get('/users', (req, res) => {
-  res.json("Hi")
+router.get('/',(req,res) => {
+ res.redirect(301,"https://witb.bradi.sh/")
+})
+
+router.get('/games', (req, res) => {
+  gamesQuery().then(res.json.bind(res))
 })
 
 router.get('/users/:userId', (req, res) => {
@@ -22,9 +33,39 @@ router.get('/users/:userId', (req, res) => {
 
   if (!user) return res.status(404).json({})
 
-  return res.json(user)
 })
 
 app.use('/', router)
 
 module.exports = app
+
+
+//dynamo
+
+const dynamoQuery = (inputModifier, outputModifier)=>new Promise((resolve,reject)=>{
+    console.log(`dynamoQuery START`)
+    let params = {
+        TableName: "witb"
+    }
+    params = inputModifier(params)
+    console.log(`Query ${JSON.stringify(params)}`)
+    return dynamo.query(params).promise()
+    .then((result)=>resolve(outputModifier(result)))
+    .catch(reject)
+})
+
+
+const standardQuery = (key,value)=>(params)=>{
+	params.ExpressionAttributeValues = {":key":value},
+	params.KeyConditionExpression =  `${key} = :key`
+	return params
+}
+
+const queryOutput = (output)=>{
+  console.log(`queryOutput START`)
+  console.log(`queryOutput ${JSON.stringify(output)}`)
+  return output.Items
+}
+
+
+const gamesQuery=()=>dynamoQuery(standardQuery("recordType","GAME"),queryOutput)
