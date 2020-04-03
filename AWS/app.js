b33507dc45b8c76ec4a5e6ea9662229c82b781d4
r1ch <rich@bradi.sh>
2020-04-03 09:37:05 +0100

    Add files via upload

---
+++ AWS/app.js
@@ -0,0 +1,30 @@
+'use strict'
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const compression = require('compression')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const app = express()
+const router = express.Router()
+
+router.use(cors())
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(awsServerlessExpressMiddleware.eventContext())
+
+router.get('/users', (req, res) => {
+  res.json("Hi")
+})
+
+router.get('/users/:userId', (req, res) => {
+  const user = getUser(req.params.userId)
+
+  if (!user) return res.status(404).json({})
+
+  return res.json(user)
+})
+
+app.use('/', router)
+
+module.exports = app
