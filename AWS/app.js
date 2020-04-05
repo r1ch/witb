c40a98a5a5650f17e20b4d73bf5b4edb5fa8e885
r1ch <rich@bradi.sh>
2020-04-05 11:27:11 +0100

    Fuck this

--- AWS/app.js
+++ AWS/app.js
@@ -25,15 +25,25 @@ router.get('/',(req,res) => {
 })
 
 router.get('/games', (req, res) => {
-  gamesQuery().then(res.json.bind(res))
+  getGames()
+  .then(res.json.bind(res))
 })
 
-router.get('/users/:userId', (req, res) => {
-  const user = getUser(req.params.userId)
-
-  if (!user) return res.status(404).json({})
-
+router.get('/games/:game/players', (req, res) => {
+  getPlayersInGame(req.params.game)
+  .then(res.json.bind(res))
 })
+
+router.put('/games/:game/players', (req, res)=>{
+  putPlayerInGame(req.body,req.params.game)
+  .then(res.json.bind(res))
+})
+
+router.put('/games/:game/players/:player/names', (req, res)=>{
+  putNamesForPlayerInGame(req.body,req.params.player,req.params.game)
+  .then(res.json.bind(res))
+})
+
 
 app.use('/', router)
 
@@ -42,30 +52,108 @@ module.exports = app
 
 //dynamo
 
-const dynamoQuery = (inputModifier, outputModifier)=>new Promise((resolve,reject)=>{
-    console.log(`dynamoQuery START`)
+
+const DB = (index)=>(funct, inputModifier, outputModifier)=>new Promise((resolve,reject)=>{
+    console.log(`DB ${funct}`)
     let params = {
         TableName: "witb"
     }
-    params = inputModifier(params)
-    console.log(`Query ${JSON.stringify(params)}`)
-    return dynamo.query(params).promise()
-    .then((result)=>resolve(outputModifier(result)))
+    if(index) params.IndexName="index"
+    return dynamo[funct](inputModifier(params)).promise()
+    .then((result)=>{
+	console.log(`DB ${funct} ${JSON.stringify(result)}`)
+	resolve(outputModifier(result))
+     })
     .catch(reject)
 })
 
 
-const standardQuery = (key,value)=>(params)=>{
-	params.ExpressionAttributeValues = {":key":value},
-	params.KeyConditionExpression =  `${key} = :key`
+const hashQuery = (hash)=>(params)=>{
+	console.log(`hashQuery ${JSON.stringify(hash)}`)
+	params.ExpressionAttributeValues = {":key":hash.value},
+	params.KeyConditionExpression =  `${hash.key} = :key`
+	console.log(`hashQuery ${JSON.stringify(params)}`)
+	return params
+}
+
+const hashBeginsQuery = (hash,sort)=>(params)=>{
+	console.log(`hashBeginsQuery ${JSON.stringify(hash)}, ${JSON.stringify(sort)}`)
+	params.ExpressionAttributeValues = {":hash":hash.value, ":sort":sort.value},
+	params.KeyConditionExpression =  `${hash.key} = :hash AND begins_with(${sort.key}, :sort)`
+	console.log(`hashQuery ${JSON.stringify(params)}`)
+	return params
+}
+
+const listAppendUpdate = (hash,sort,list,item)=>(params)=>{
+	console.log(`listAppendUpdate ${JSON.stringify(hash)}, ${JSON.stringify(sort)}, ${JSON.stringify(item)}`)
+	params.Key = {[`${hash.key}`]: hash.value, [`${sort.key}`]: sort.value}
+	params.UpdateExpression = "add #list :item"
+	params.ExpressionAttributeNames = {"#list": list}
+	params.ExpressionAttributeValues = {":item": dynamo.createSet([item])}
+	params.ReturnValues = "ALL_NEW"
+	console.log(`listAppendUpdate ${JSON.stringify(params)}`)
+	return params 
+}
+
+const standardUpdate = (hash,sort,item)=>(params)=>{
+	console.log(`standardUpdate ${JSON.stringify(hash)}, ${JSON.stringify(sort)}, ${JSON.stringify(item)}`)
+	params.Key = {[`${hash.key}`]: hash.value, [`${sort.key}`]: sort.value}
+	params.UpdateExpression =  "set details = :details"
+	params.ExpressionAttributeValues = {
+		":details":item,
+	},
+	params.ReturnValues = "ALL_NEW"
+	console.log(`standardUpdate ${JSON.stringify(params)}`)
 	return params
 }
 
 const queryOutput = (output)=>{
-  console.log(`queryOutput START`)
   console.log(`queryOutput ${JSON.stringify(output)}`)
-  return output.Items
+  return output.Items.map(item=>{
+	item.details.recordType = item.recordType
+	item.details.recordId = item.identifier
+	return item.details
+  })
+}
+
+const updateOutput = (output)=>{
+  console.log(`updateOutput ${JSON.stringify(output)}`)
+  output.Attributes.details.recordType = output.Attributes.recordType
+  output.Attributes.details.recordId = output.Attributes.identifier
+  return output.Attributes.details
 }
 
 
-const gamesQuery=()=>dynamoQuery(standardQuery("recordType","GAME"),queryOutput)
+const t = (key,value)=>({key:key,value:value})
+
+
+const getGames=()=>DB()(
+			"query",
+			hashQuery(t("recordType","GAME")),
+			queryOutput
+			)
+
+const getPlayersInGame=(game)=>DB()(
+			"query",
+			hashBeginsQuery(t("recordType","PLAYER"),t("identifier",game)),
+			queryOutput
+			)
+
+/*const putPlayerInGame=(player,game)=>DB()(
+			"update",
+			standardUpdate(t("recordType","PLAYER"),t("identifier",`${game}:${player.id}`),player),
+			updateOutput
+			)*/
+
+const putPlayerInGame=(player,game)=>DB()(
+			"update",
+			listAppendUpdate(t("recordType","GAME"),t("identifier",game),"details.players",player.id),
+			updateOutput
+			)
+
+
+const putNamesForPlayerInGame=(names,player,game)=>DB()(
+			"update",
+			standardUpdate(t("recordType","NAMES"),t("identifier",`${game}:${player}`),names),
+			updateOutput
+			)
