e6d8ae3f3e8451dda53ea78e2745db5cc6d76446
r1ch <rich@bradi.sh>
2020-04-07 20:31:37 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,16 +20,13 @@ Vue.component('google-login', {
 	}),
 	template: `
 		<div class = "row">
-			!{{$profile}}!
 			<div v-if = "!authenticated" class="g-signin2" data-width="200" data-height="50" data-onsuccess="authenticate" data-theme="dark"></div>
-			<img v-if = "$profile" :src="$profile.url"></img>
 		</div>
 	`,
 	mounted: function() {
 		Credentials.then((profile) => {
-			console.log(profile)
 			this.authenticated = true;
-			Vue.prototype.$profile = profile
+			this.$emit("profileReady",profile)
 		})
 	}
 })
@@ -123,5 +120,16 @@ Vue.component('witb-name',{
 var app = new Vue({
 	el: '#app',
 	data: {
+		profile: false;
+	},
+	methods:{
+		profileReady(event){
+			this.profile = event
+		}
+	},
+	provide: function(){
+		return {
+			profile: this.profile
+		}
 	}
-})
+})	
