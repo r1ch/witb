fc904a94842a5ea944c9462628b991c5672930ad
r1ch <rich@bradi.sh>
2020-04-08 13:39:28 +0100

    MmM

--- AWS/app.js
+++ AWS/app.js
@@ -3,7 +3,18 @@
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient()
 
+//Dynamo Setup
+const {
+    DynamoDbSchema,
+    DynamoDbTable,
+    DataMapper,
+    embed
+} = require('@aws/dynamodb-data-mapper');
+const { v4 } = require('uuid');
+const DynamoDB = require('aws-sdk/clients/dynamodb');
 
+const client = new DynamoDB({region: 'eu-west-1'});
+const mapper = new DataMapper({client});
 
 //Express Setup
 const path = require('path')
@@ -14,146 +25,171 @@ const compression = require('compression')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const app = express()
 const router = express.Router()
+const asyncHandler = require('express-async-handler')
+
 
 router.use(cors())
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 router.use(awsServerlessExpressMiddleware.eventContext())
 
+
 router.get('/',(req,res) => {
- res.redirect(301,"https://witb.bradi.sh/")
+    res.redirect(301,"https://witb.bradi.sh/")
 })
 
-router.get('/games', (req, res) => {
-  getGames()
-  .then(res.json.bind(res))
-})
+router.get('/games', asyncHandler(async (req, res) => {
+    let games = []
+    for await (const game of mapper.query(Game, {recordType: 'GAME'})) {
+        games.push(game)
+    }
+    res.json(games)
+}))
 
-router.get('/games/:game/players', (req, res) => {
-  getPlayersInGame(req.params.game)
-  .then(res.json.bind(res))
+router.get('/games/:game', (req, res) => {
+    mapper.get(Object.assign(new Game, {identifier: req.params.game}))
+    .then(res.json.bind(res))
+    .catch(err => {
+        res.status(404).send(`No game found for ID: ${req.params.game}`)
+    })
 })
 
 router.put('/games/:game/players', (req, res)=>{
-  putPlayerInGame(req.body,req.params.game)
-  .then(res.json.bind(res))
+    let player = new Player(req.body.id);
+    player.url = req.body.url
+    player.name = req.body.name
+    player.association = req.params.game;
+    mapper.update(player,{onMissing: 'skip'}).then(res.json.bind(res))
 })
+
+router.get('/games/:game/players', asyncHandler(async (req, res) => {
+    let players = []
+    for await (const player of mapper.query(Player, {recordType: 'PLAYER', association: req.params.game}, {indexName: 'index'})) {
+        players.push(simple(player))
+    }
+    res.json(players)
+}))
+
+router.get('/games/:game/players/:player/names', asyncHandler(async (req, res) => {
+    mapper.get(Object.assign(new Player, {identifier: req.params.player}))
+    .then((player)=>{
+        console.log(JSON.stringify(player))
+        res.json(namesOf(player))
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(404).send(`No names found for player with ID: ${req.params.player}, ${err}`)
+    })
+}))
 
 router.put('/games/:game/players/:player/names', (req, res)=>{
-  putNamesForPlayerInGame(req.body,req.params.player,req.params.game)
-  .then(res.json.bind(res))
+    let player = new Player(req.params.player);
+    player.names = req.body
+    mapper.update(player,{onMissing: 'skip'}).then((player)=>{
+        res.json(namesOf(player))
+    })
 })
 
+router.get('/games/:game/names', asyncHandler(async (req, res) => {
+    let names = []
+    for await (const player of mapper.query(Player, {recordType: 'PLAYER', association: req.params.game}, {indexName: 'index'})) {
+        names.push(...namesOf(player))
+    }
+    res.json(names)
+}))
+
+router.put('/players', (req, res)=>{
+    let player = new Player(req.body.id);
+    player.url = req.body.url
+    player.name = req.body.name
+    mapper.update(player,{onMissing: 'skip'}).then(res.json.bind(res))
+})
+
+router.get('/players', asyncHandler(async (req, res) => {
+    let players = []
+    for await (const player of mapper.query(Player, {recordType: 'PLAYER'})) {
+        players.push(simple(player))
+    }
+    res.json(players)
+}))
 
 app.use('/', router)
 
 module.exports = app
 
 
-//dynamo
+const simple = (player)=>{
+    console.log(`Simplify Player ${JSON.stringify(player)}`)
+    if (player.names) player.numberOfNames = player.names.length
+    else player.numberOfNames = 0
+    delete player.names
+    return player
+}
 
+const namesOf = (player)=>{
+    if (player.names) return player.names
+    else return []
+}
 
-const DB = (index)=>(funct, inputModifier, outputModifier)=>new Promise((resolve,reject)=>{
-    console.log(`DB ${funct}`)
-    let params = {
-        TableName: "witb"
+class Game {
+    constructor(title){
+        this.title = title
     }
-    if(index) params.IndexName="index"
-    return dynamo[funct](inputModifier(params)).promise()
-    .then((result)=>{
-	console.log(`DB ${funct} ${JSON.stringify(result)}`)
-	resolve(outputModifier(result))
-     })
-    .catch(reject)
-})
-
-
-const hashQuery = (hash)=>(params)=>{
-	console.log(`hashQuery ${JSON.stringify(hash)}`)
-	params.ExpressionAttributeValues = {":key":hash.value},
-	params.KeyConditionExpression =  `${hash.key} = :key`
-	console.log(`hashQuery ${JSON.stringify(params)}`)
-	return params
 }
 
-const hashBeginsQuery = (hash,sort)=>(params)=>{
-	console.log(`hashBeginsQuery ${JSON.stringify(hash)}, ${JSON.stringify(sort)}`)
-	params.ExpressionAttributeValues = {":hash":hash.value, ":sort":sort.value},
-	params.KeyConditionExpression =  `${hash.key} = :hash AND begins_with(${sort.key}, :sort)`
-	console.log(`hashQuery ${JSON.stringify(params)}`)
-	return params
+Object.defineProperties(Game.prototype, {
+    [DynamoDbTable]: {
+        value: 'witb'
+    },
+    [DynamoDbSchema]: {
+        value: {
+            recordType: {
+                type: 'String',
+                keyType: 'HASH',
+                defaultProvider: ()=>"GAME",
+            },
+            identifier: {
+	              type: 'String',
+                  keyType: 'RANGE',
+                  defaultProvider: v4
+            },
+            title: {type : 'String'},
+            rounds: {type: 'List', memberType: {type: 'String'}},
+            secondsPerRound: {type: 'Number'},
+            namesPerPerson: {type: 'Number'}
+        },
+    },
+});
+
+
+class Player {
+    constructor(identifier){
+        this.identifier = identifier
+    }
 }
 
-const listAppendUpdate = (hash,sort,list,item)=>(params)=>{
-	console.log(`listAppendUpdate ${JSON.stringify(hash)}, ${JSON.stringify(sort)}, ${JSON.stringify(item)}`)
-	params.Key = {[`${hash.key}`]: hash.value, [`${sort.key}`]: sort.value}
-	params.UpdateExpression = "add #list :item"
-	params.ExpressionAttributeNames = {"#list": list}
-	params.ExpressionAttributeValues = {":item": dynamo.createSet([item])}
-	params.ReturnValues = "ALL_NEW"
-	console.log(`listAppendUpdate ${JSON.stringify(params)}`)
-	return params 
-}
-
-const standardUpdate = (hash,sort,item)=>(params)=>{
-	console.log(`standardUpdate ${JSON.stringify(hash)}, ${JSON.stringify(sort)}, ${JSON.stringify(item)}`)
-	params.Key = {[`${hash.key}`]: hash.value, [`${sort.key}`]: sort.value}
-	params.UpdateExpression =  "set details = :details"
-	params.ExpressionAttributeValues = {
-		":details":item,
-	},
-	params.ReturnValues = "ALL_NEW"
-	console.log(`standardUpdate ${JSON.stringify(params)}`)
-	return params
-}
-
-const queryOutput = (output)=>{
-  console.log(`queryOutput ${JSON.stringify(output)}`)
-  return output.Items.map(item=>{
-	item.details.recordType = item.recordType
-	item.details.recordId = item.identifier
-	return item.details
-  })
-}
-
-const updateOutput = (output)=>{
-  console.log(`updateOutput ${JSON.stringify(output)}`)
-  output.Attributes.details.recordType = output.Attributes.recordType
-  output.Attributes.details.recordId = output.Attributes.identifier
-  return output.Attributes.details
-}
-
-
-const t = (key,value)=>({key:key,value:value})
-
-
-const getGames=()=>DB()(
-			"query",
-			hashQuery(t("recordType","GAME")),
-			queryOutput
-			)
-
-const getPlayersInGame=(game)=>DB()(
-			"query",
-			hashBeginsQuery(t("recordType","PLAYER"),t("identifier",game)),
-			queryOutput
-			)
-
-/*const putPlayerInGame=(player,game)=>DB()(
-			"update",
-			standardUpdate(t("recordType","PLAYER"),t("identifier",`${game}:${player.id}`),player),
-			updateOutput
-			)*/
-
-const putPlayerInGame=(player,game)=>DB()(
-			"update",
-			listAppendUpdate(t("recordType","GAME"),t("identifier",game),"details.players",player.id),
-			updateOutput
-			)
-
-
-const putNamesForPlayerInGame=(names,player,game)=>DB()(
-			"update",
-			standardUpdate(t("recordType","NAMES"),t("identifier",`${game}:${player}`),names),
-			updateOutput
-			)
+Object.defineProperties(Player.prototype, {
+    [DynamoDbTable]: {
+        value: 'witb'
+    },
+    [DynamoDbSchema]: {
+        value: {
+            recordType: {
+                type: 'String',
+                keyType: 'HASH',
+                defaultProvider: ()=>"PLAYER",
+            },
+            identifier: {
+	              type: 'String',
+                  keyType: 'RANGE',
+                  defaultProvider: v4   
+            },
+            name: {type : 'String'},
+            url: {type: 'String'},
+            association: {type: 'String'},
+            names: {
+                type: 'List', memberType: {type: 'String'}
+            }
+        },
+    },
+});
