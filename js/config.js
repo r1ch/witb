8edd0f729bc10bfb5b2209b1ab1312c7ee3c2867
r1ch <rich@bradi.sh>
2020-04-13 21:07:27 +0100

    new endpoints

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window.config = {
     roleArn: "arn:aws:iam::312837731527:role/GoogleAssumeWITBRole",
     region: "eu-west-1",
-    apiGatewayUrl : "https://3a6pehhpyd.execute-api.eu-west-1.amazonaws.com",
+    apiGatewayUrl : "https://w0imliph9e.execute-api.eu-west-1.amazonaws.com",
     apiGatewayPath : "/prod",
-    socketGatewayUrl : "wss://qmk0abhxji.execute-api.eu-west-1.amazonaws.com",
+    socketGatewayUrl : "wss://cqdmdfmogl.execute-api.eu-west-1.amazonaws.com",
     socketGatewayPath: "/prod"
 }
