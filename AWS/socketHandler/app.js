5e9e3a6a3f75785d116b3c9a59ff38f777984fa0
r1ch <rich@bradi.sh>
2020-04-14 08:51:29 +0100

    socket handling

--- AWS/socketHandler/app.js
+++ AWS/socketHandler/app.js
@@ -1,32 +1,47 @@
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
+const apigwManagementApi = new AWS.ApiGatewayManagementApi({endpoint: process.env.ENDPOINT});
 
 exports.handler = async event => {
  console.log(event)
 
  switch(event.requestContext.routeKey){
    case '$connect':
-     return await ddb.put(connect(event)).promise()
+     return await ddb.put(connect(event.requestContext.connectionId)).promise()
      .then(success("Connected"))
      .catch(error)
    case '$disconnect':
-      return await ddb.delete(disconnect(event)).promise()
+      return await ddb.delete(disconnect(event.requestContext.connectionId)).promise()
       .then(success("Disconnected"))
       .catch(error)
    case 'sendmessage':
-      return success("Hi!")
+      return await ddb.query(connections()).promise()
+      .then(sendToConnections("Hi"))
+      .then(success("Sent to all"))
+      .catch(error)  
    default:
      return error("GFY")
  }
 };
 
+const sendToConnections = async (message) => async (connections) => {
+  let postBag = connections.map((connection)=>{
+    await apigwManagementApi.postToConnection({ ConnectionId: connection, Data: message }).promise()
+    .catch((err)=>{
+      if(err.statusCode == 410) {
+        console.log(`Clean up stale connection : ${connection}`)
+        await ddb.delete(disconnect(connection)).promise();
+      } else throw err
+    })
+  })
+  return Promise.all(postBag)
+}
 
-
-const connect = (event)=>({
+const connect = (id)=>({
   TableName: 'witb',
   Item: {
     recordType : "SOCKET",
-    identifier : event.requestContext.connectionId
+    identifier : id
   }
 })
 
@@ -38,11 +53,23 @@ const error = (error)=>({
   statusCode: 200, body: JSON.stringify(error)
 })
 
+const connections = ()=>({
+    TableName : 'witb',
+    KeyConditionExpression: "#recordType = :socket",
+    ProjectionExpression: "identifier",
+    ExpressionAttributeNames:{
+        "#recordType": "recordType"
+    },
+    ExpressionAttributeValues: {
+        ":socket": "SOCKET"
+    }
+})
 
-const disconnect = (event)=>({
+
+const disconnect = (id)=>({
   TableName: 'witb',
   Key: {
     recordType : "SOCKET",
-    identifier : event.requestContext.connectionId
+    identifier : id
   }
 })
